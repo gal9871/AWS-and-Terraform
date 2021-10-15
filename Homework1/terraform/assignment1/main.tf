45dbccfe099052595b90581ec7ad1e1f83616d1e
gal9871 <gal456456@gmail.com>
2021-10-16 02:34:13 +0300

    bla

--- Homework1/terraform/assignment1/main.tf
+++ Homework1/terraform/assignment1/main.tf
@@ -2,3 +2,38 @@ resource "aws_default_vpc" "default" {
 
 }
 
+module "nginx-sg" {
+  source      = "..\\modules\\security-group"
+  name        = "nginx-server-sg"
+  description = "allow ssh,http"
+  vpc_id      = aws_default_vpc.default.id
+  ingress = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }]
+  egress = [{
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }]
+
+    tags = {
+    Name = "allow_http-ssh",
+    Owner = "Gal",
+    Purpuse = "SG for NGINX"
+  }
+}
+
+# module "nginx-instance-1"{
+#     source = "..\\modules\\ec2-instance"
+# }
--- Homework1/terraform/modules/security-group/variables.tf
+++ Homework1/terraform/modules/security-group/variables.tf
@@ -1,16 +1,3 @@
-variable "egress" {
-  type = map
-}
-
-variable "ingress" {
-  type = map
-  default = {
-      from_port = 0
-      to_port = 0
-      protocol = -1
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 variable "description" {
   type = string
@@ -22,4 +9,8 @@ variable "name" {
 
 variable "vpc_id" {
     type = string
+}
+
+variable "tags" {
+  type = map
 }
