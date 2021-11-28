40bae0e6b217d061cca6d93ff1a8e95586f9df2f
gal9871 <gal456456@gmail.com>
2021-11-28 21:01:55 +0200

    hw

--- Homework3/main.tf
+++ Homework3/main.tf
@@ -335,7 +335,8 @@ module "nginx_instance_profile" {
 }
 
 module "ansible_sg"{
-  source = "github.com/gal9871/configuration-management.git//materials/terraform?ref=5e6923c998e687163c44937eaf7de254698d6a7b"
+  source = "..\\..\\configuration-management\\materials\\terraform"
+ # source = "github.com/gal9871/configuration-management.git//materials/terraform?ref=d30233c954e518048c3c2c0ccc36a50127f6b82f"
   vpc_id        = module.main_vpc.aws_vpc_id
   subnet_id     = module.public_subnet_1.aws_subnet_id
 }
