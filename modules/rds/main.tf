3f2a718db5d78bd3fe472f9a01e60231091f1ef9
gal9871 <gal456456@gmail.com>
2022-02-19 13:07:25 +0200

    rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,13 +1,13 @@
 resource "aws_db_instance" "default" {
-  allocated_storage     = 20
-  max_allocated_storage = 1000
-  engine                = "postgres"
-  engine_version        = "13.4"
-  instance_class        = "db.t3.micro"
-  db_name               = "mydb"
-  username              = "postgres"
+  allocated_storage     = var.allocated_storage
+  max_allocated_storage = var.max_allocated_storage
+  engine                = var.engine
+  engine_version        = var.engine_version
+  instance_class        = var.instance_class
+  db_name               = var.db_name
+  username              = var.username
   password              = jsondecode(data.aws_secretsmanager_secret_version.postgres_password.secret_string)["password"]
-  parameter_group_name  = "default.postgres13"
+  parameter_group_name  = var.parameter_group_name
   skip_final_snapshot   = true
   publicly_accessible   = true
   port                  = 5432
