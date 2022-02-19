82624f7799861ad5c9fb3613c72b8f19ecb1664f
gal9871 <gal456456@gmail.com>
2022-02-19 12:58:31 +0200

    midproj

--- midProj/consul.tf
+++ midProj/consul.tf
@@ -1,5 +1,7 @@
 module "consul" {
-  source    = "C:\\Users\\gal98\\OpsSchool-new\\service_discovery\\solutions\\terraform"
+  #source    = "C:\\Users\\gal98\\OpsSchool-new\\service_discovery\\solutions\\terraform"
+  source = "github.com/gal9871/service_discovery.git//solutions/terraform?ref=6e714b9aaa3a553a64816e1e21fe4dc7ad35e1b3"
+
   subnet_id = [module.private_subnet_1.aws_subnet_id, module.private_subnet_2.aws_subnet_id]
   vpc_id    = module.main_vpc.aws_vpc_id
 }
