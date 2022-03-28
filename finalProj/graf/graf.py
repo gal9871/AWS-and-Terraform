67ba508052cd6460ecf53ce91a65b527f4287fe5
gal9871 <gal456456@gmail.com>
2022-03-28 20:53:15 +0300

    ASDF

--- finalProj/graf/graf.py
+++ finalProj/graf/graf.py
@@ -13,8 +13,8 @@ for filename in os.listdir(directory):
         headers = CaseInsensitiveDict()
         headers["Accept"] = "application/json"
         headers["Content-Type"] = "application/json"
-        headers["Authorization"] = "Bearer eyJrIjoiczFKV05aeUwwWXV0NTkwN2RNS0Mxb3duN1lBYzZQbHQiLCJuIjoiamoiLCJpZCI6MX0="
-        url = 'http://app-lb-1241628419.us-east-1.elb.amazonaws.com:3000/api/dashboards/db'
+        headers["Authorization"] = "Bearer eyJrIjoidm1OM1dmN2VGSWhLZ0c5ODFEQ2oyUmNjYVRoUDZqeUsiLCJuIjoiamoiLCJpZCI6MX0="
+        url = 'http://app-lb-939330077.us-east-1.elb.amazonaws.com:3000/api/dashboards/db'
         x = requests.post(url, json = data, headers = headers)
 
         print(x.text)
